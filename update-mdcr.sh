3cb71e161e7c75dd89dd994c9fc7e67d77881f77
Allan Wang <me@allanwang.ca>
2019-05-22 19:56:51 -0700

    Support mdcr imports, see https://github.com/facebook/create-react-app/pull/5599#issuecomment-441464697

---
+++ update-mdcr.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+npm i -S \
+@material/react-button@latest \
+@material/react-card@latest \
+@material/react-checkbox@latest \
+@material/react-fab@latest \
+@material/react-icon-button@latest \
+@material/react-material-icon@latest \
+@material/react-ripple@latest \
+@material/react-text-field@latest \
+@material/react-top-app-bar@latest \
+@material/react-typography@latest \
